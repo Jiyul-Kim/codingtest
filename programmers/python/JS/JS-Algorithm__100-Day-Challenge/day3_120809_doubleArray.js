2ae87ced7a377626614d2719b26da90d18f7cfce
KimJiYul <kjekje041212@gmail.com>
2023-05-10 12:44:32 +0900

    [codingTest] - 120809, 120822 문제 해결

---
+++ programmers/python/JS/JS-Algorithm__100-Day-Challenge/day3_120809_doubleArray.js
@@ -0,0 +1,16 @@
+// 코딩테스트 입문 > 배열 두 배 만들기
+
+// 정수 배열 numbers가 매개변수로 주어집니다. numbers의 각 원소에 두배한 원소를 가진 배열을 return하도록 solution 함수를 완성해주세요.
+
+function solution(numbers) {
+    return doubleArray = numbers.map (double = num => num * 2) ;
+    
+}
+
+solution([1, 2, 3, 4, 5])
+
+// 앗 좀 더 하드코딩 식으로 쓰자면 이렇게 쓸 수 있네요.
+function solution(numbers) {
+    return doubleArray = numbers.map (num => num * 2) ;
+    
+}
---
+++ programmers/python/JS/JS-Algorithm__100-Day-Challenge/day3_120826_removeSpecificChar.js
@@ -0,0 +1,14 @@
+// 코딩테스트 입문 > 특정 문자 제거하기
+// 문자열 my_string과 문자 letter이 매개변수로 주어집니다. my_string에서 letter를 제거한 문자열을 return하도록 solution 함수를 완성해주세요.
+
+// 문제 해결 못함. 
+// 해결하려했던 과정
+// 1. replace
+// > replace는 앞에 한 글자만 바뀌어서 실패
+// 2. replace를 반복하는 방식
+// > while로 하려 했으나 조건을 어떻게 해야할지 몰라서 실패 
+ 
+function solution(my_string, letter) {
+    return my_string.replaceAll(letter, ""); 
+}
+solution("BCBdbe", "B")
